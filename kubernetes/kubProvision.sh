53bc5c7edff566781668db105d4e10b9c46e3c34
austin-1210 <jinkyun1210@gmail.com>
2019-07-17 16:42:11 +0900

    modified:   kubernetes/kubProvision.sh 	new file:   kubernetes/service.yaml

--- kubernetes/kubProvision.sh
+++ kubernetes/kubProvision.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # kubectl run nginx1 --image=jinkyun1210/nginx --port=80
 # kubectl scale depolyment nginx --replicas=4
-# kubectl expose deployment/nginx --type="NodePort" --port 80
 
 kubectl delete deploy/nginx1
 kubectl create -f ./deploy.yaml
+
+kubectl delete svc/nginx1
+# kubectl expose deployment/nginx --type="NodePort" --port 80
+kubectl create -f ./service.yaml
+
